a611bd06a14765050b7c3be4f1dd2c0c3b068bb1
ahsanulks <ahsanulkh996@gmail.com>
2024-02-04 07:47:50 +0000

    feat: add config for adding db connection

---
+++ adapter/driven/postgresql.go
@@ -0,0 +1,25 @@
+// This file contains the repository implementation layer.
+package driven
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/SawitProRecruitment/UserService/config"
+	_ "github.com/lib/pq"
+)
+
+type PostgreConnection struct {
+	Db *sql.DB
+}
+
+func NewPostgreConnection(c *config.DBConfig) *PostgreConnection {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable", c.User, c.Password, c.Hostname, c.Port, c.DB)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic(err)
+	}
+	return &PostgreConnection{
+		Db: db,
+	}
+}
--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -4,7 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users
 (
     id              UUID         PRIMARY KEY DEFAULT gen_random_uuid(),
-    phone_number    VARCHAR(13)  NOT NULL,
+    phone_number    VARCHAR(13)  UNIQUE NOT NULL,
     full_name       VARCHAR(64)  NOT NULL,
     password        VARCHAR(255) NOT NULL,
     created_at      TIMESTAMPTZ  DEFAULT NOW(),
