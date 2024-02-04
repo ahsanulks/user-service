a39bbf3f95c480806fb6cec65dc89b53074627d0
ahsanulks <ahsanulkh996@gmail.com>
2024-02-04 13:27:28 +0000

    test: add case when success created

--- handler/create_user_test.go
+++ handler/create_user_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/SawitProRecruitment/UserService/generated"
 	"github.com/SawitProRecruitment/UserService/test/fake"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
@@ -98,4 +99,33 @@ func TestServer_CreateUser_UnexpectedError(t *testing.T) {
 
 	assert.Equal(http.StatusInternalServerError, rec.Code)
 	assert.Equal("InternalServerError", response.Type)
+}
+
+func TestServer_CreateUser_Success(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/api/users", strings.NewReader(`{"PhoneNumber": "123456789", "FullName": "John Doe", "Password": "password123"}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	rec := httptest.NewRecorder()
+	ctx := e.NewContext(req, rec)
+	assert := assert.New(t)
+
+	fu := fake.NewFakeUserUsecase()
+	server := &Server{
+		uu: fu,
+	}
+
+	err := server.CreateUser(ctx)
+	assert.NoError(err)
+
+	var response generated.CreateUserResponse
+	err = json.Unmarshal(rec.Body.Bytes(), &response)
+
+	assert.Equal(http.StatusCreated, rec.Code)
+	assert.NoError(err)
+	assert.NotEqual(uuid.Nil, response.Id)
+
+	gotID, err := fu.GetIDByPhone("123456789")
+	assert.NoError(err)
+	assert.Equal(gotID, response.Id.String())
 }
