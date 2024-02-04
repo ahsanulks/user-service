dc1bde20099c434bcaa1b1ddc48039f4a4f234b3
ahsanulks <ahsanulkh996@gmail.com>
2024-02-04 08:05:10 +0000

    test: add case when success insert user should return id

---
+++ adapter/driven/user_db.go
@@ -0,0 +1,25 @@
+package driven
+
+import (
+	"context"
+
+	"github.com/SawitProRecruitment/UserService/internal/user/entity"
+	"github.com/SawitProRecruitment/UserService/internal/user/port/driven"
+)
+
+var _ driven.UserWriter = new(UserDB)
+
+type UserDB struct {
+	conn *PostgreConnection
+}
+
+func NewUserDB(db *PostgreConnection) *UserDB {
+	return &UserDB{
+		conn: db,
+	}
+}
+
+// Create implements driven.UserWriter.
+func (udb *UserDB) Create(ctx context.Context, user *entity.User) (id string, err error) {
+	return
+}
